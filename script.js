268547ad28d904dad046321678a6dd711c5b47e3
alexsigei <alex.sigei@student.moringaschool.com>
2026-07-14 19:09:32 +0300

    Declared all the Document elements

---
+++ script.js
@@ -0,0 +1,12 @@
+const form = document.getElementById("searchForm");
+const input = document.getElementById("wordInput");
+
+const word = document.getElementById("word");
+const phonetic = document.getElementById("phonetic");
+const partSpeech = document.getElementById("partSpeech");
+const definition = document.getElementById("definition");
+const example = document.getElementById("example");
+const synonyms = document.getElementById("synonyms");
+const error = document.getElementById("error");
+const results = document.getElementById("results");
+const audioBtn = document.getElementById("audioBtn");
